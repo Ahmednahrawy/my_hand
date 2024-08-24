c15f4b8933cf1955fc55ab2736abebf83894d708
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-08-24 19:17:40 +0300

    modify theming files

---
+++ lib/core/widgets/text_form_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_hand/config/theme/colors.dart';
+import 'package:my_hand/config/theme/styles.dart';
+
+class AppTextFormField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  final TextEditingController? controller;
+  final Function(String?) validator;
+
+  const AppTextFormField({
+    super.key,
+    this.contentPadding,
+    this.focusedBorder,
+    this.enabledBorder,
+    this.inputTextStyle,
+    this.hintStyle,
+    required this.hintText,
+    this.isObscureText,
+    this.suffixIcon,
+    this.backgroundColor,
+    this.controller,
+    required this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(
+              horizontal: 20.w,
+              vertical: 18.h,
+            ),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: ColorsManager.mainBlue,
+                  width: 1.3,
+                ),
+                borderRadius: BorderRadius.circular(16)),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide:
+                  const BorderSide(color: ColorsManager.lightBlue, width: 1.3),
+              borderRadius: BorderRadius.circular(16),
+            ),
+        hintStyle: hintStyle ?? TextStyles.font14LightGrayRegular,
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        fillColor: backgroundColor ?? ColorsManager.moreLightGray,
+        filled: true,
+      ),
+      obscureText: isObscureText ?? false,
+      style: TextStyles.font14DarkBlueMedium,
+    );
+  }
+}
